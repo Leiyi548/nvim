c32d0fe45a2640d5389c8c881f4000093629deee
Leiyi548 <1424630446@qq.com>
2022-01-12 15:59:13 +0800

    update

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.treesitter"
-require "user.lualine"
-require "user.impatient"
-require "user.autocommands"
+require("user.colorscheme")
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.treesitter")
+require("user.lualine")
+require("user.impatient")
+require("user.autocommands")
