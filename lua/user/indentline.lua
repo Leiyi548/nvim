6a9926402bfe62ce6730fdbf4099180e02f1d160
Leiyi548 <1424630446@qq.com>
2022-01-29 23:18:02 +0800

    使用更细的indentline 更像vscode

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -17,8 +17,8 @@ vim.g.indent_blankline_filetype_exclude = {
 	"TelescopeResults",
 }
 vim.g.indentLine_enabled = 1
--- vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_char = "▏"
+vim.g.indent_blankline_char = "│"
+-- vim.g.indent_blankline_char = "▏"
 -- vim.g.indent_blankline_char = "▎"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
