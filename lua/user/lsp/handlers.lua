8c09b393b1b87d92e50745a3ac1a8bf40a5bb8b4
Leiyi548 <1424630446@qq.com>
2022-02-09 16:14:57 +0800

    disable html format --> use null_ls format

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -88,6 +88,9 @@ M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
 		client.resolved_capabilities.document_formatting = false
 	end
+	if client.name == "html" then
+		client.resolved_capabilities.document_formatting = false
+	end
 	lsp_keymaps(bufnr)
 	-- lsp_highlight_document(client)
 end
