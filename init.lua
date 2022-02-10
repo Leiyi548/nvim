6c4e8ab9ee3cbf5edecfc0ea538bf7495f2b0f63
Leiyi548 <1424630446@qq.com>
2022-02-10 16:49:46 +0800

    update README.md

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ builtin.emmet = { active = false } -- enable to use emmet plugins
 builtin.tabnine = { active = false } -- enable to use emmet plugins
 builtin.smartNumber = { active = true } -- enable to open smartNumber, insert mode close relative number,normal mode open relative number
 builtin.persistence = { active = true } -- enable to open persistence for session management
+builtin.neorg = { active = true } -- enable to open neorg to noteTaking
 
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --━━━━━━━━━━━━━━━━━━━━━❰ end  builtin configs ❱━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
