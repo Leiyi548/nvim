3815cec1219e49bfe8092af6dc26cc9eaf8c9208
Leiyi548 <1424630446@qq.com>
2022-02-16 17:49:51 +0800

    remove jsonls format use null-ls format

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -91,6 +91,9 @@ M.on_attach = function(client, bufnr)
 	if client.name == "html" then
 		client.resolved_capabilities.document_formatting = false
 	end
+	if client.name == "jsonls" then
+		client.resolved_capabilities.document_formatting = false
+	end
 	lsp_keymaps(bufnr)
 	-- lsp_highlight_document(client)
 end
