112737222dfc07ccb401ccb0caa9872b009016fc
Leiyi548 <1424630446@qq.com>
2022-02-16 22:47:32 +0800

    disable fold in .org

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -70,4 +70,5 @@ vim.cmd([[
   autocmd FileType coffee,javascript setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120
   autocmd FileType python setlocal tabstop=4 shiftwidth=4 softtabstop=4 textwidth=120
   autocmd FileType html,htmldjango,xhtml,haml setlocal tabstop=2 shiftwidth=2 softtabst
+  autocmd FileType org setlocal nofoldenable
 ]])
