7e00ae51974c0aa3c1c57e53a32a61c377c61e12
Leiyi548 <1424630446@qq.com>
2022-02-20 14:40:03 +0800

    use q to quit startuptime

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -5,6 +5,7 @@ vim.cmd([[
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
+    autocmd FileType startuptime nnoremap <silent> <buffer> q :q<cr>" },
   augroup end
 
   augroup _git
