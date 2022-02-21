e3b821a6c4e0edef824c2744d6569f171cfe16f6
Leiyi548 <1424630446@qq.com>
2022-02-21 10:26:14 +0800

    remove orgmode.nvim

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ builtin.tabnine = { active = false } -- enable to use emmet plugins
 builtin.smartNumber = { active = false } -- enable to open smartNumber, insert mode close relative number,normal mode open relative number
 builtin.persistence = { active = true } -- enable to open persistence for session management
 builtin.neorg = { active = false } -- enable to open neorg to noteTaking
-builtin.orgmode = { active = true } -- enable to use orgmode plugins
+builtin.orgmode = { active = false } -- enable to use orgmode plugins
 builtin.cmp_autopairs = { active = true } -- enable to open cmp_autopairs.on_confirm_done
 builtin.telescope_project = { active = true } -- enable to use project.nvim plugin
 builtin.templates = { active = true } -- enable to use vim-template plugins to set file header
