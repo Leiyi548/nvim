6c046038e18d62a8bb1ef9f0b6a8cde68895fe21
Leiyi548 <1424630446@qq.com>
2022-02-23 16:29:41 +0800

    fix diagnostic icon && use float window to show outline

--- lua/user/outline.lua
+++ lua/user/outline.lua
@@ -19,7 +19,7 @@ require("aerial").setup({
 	-- Determines the default direction to open the aerial window. The 'prefer'
 	-- options will open the window in the other direction *if* there is a
 	-- different buffer in the way of the preferred direction
-	default_direction = "prefer_right",
+	default_direction = "float",
 
 	-- Disable aerial on files with this many lines
 	disable_max_lines = 10000,
