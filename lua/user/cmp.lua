8dd52e63bd8aad0f829afae29cff6a94bc28a652
Leiyi548 <1424630446@qq.com>
2022-04-22 00:02:01 +0800

    remove cmdline completion for /

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -169,12 +169,12 @@ cmp.setup({
 	},
 })
  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
+  -- cmp.setup.cmdline('/', {
+  --   mapping = cmp.mapping.preset.cmdline(),
+  --   sources = {
+  --     { name = 'buffer' }
+  --   }
+  -- })
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
