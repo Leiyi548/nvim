b1df76393bbe29a4509260042b64f11a267e6fa9
Leiyi548 <1424630446@qq.com>
2022-05-05 12:20:01 +0800

    feat: add keybinding <c-h> toggle lsp_signature float windows

--- lua/user/lsp_signature.lua
+++ lua/user/lsp_signature.lua
@@ -55,7 +55,7 @@ M.config = function()
 		shadow_blend = 36, -- if you using shadow as border use this set the opacity
 		shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
 		timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-		toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+		toggle_key = "<C-h>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 	}
 	sig.setup(cfg)
 end
