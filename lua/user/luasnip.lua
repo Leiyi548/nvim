854df3c13897cba250c4c8979286c2ce8df894de
Leiyi548 <1424630446@qq.com>
2022-05-14 09:19:31 +0800

    fix(luasnip): fix luasnip tab error

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -94,6 +94,21 @@ ls.snippets = {
 -- load vscode style snippets
 require("luasnip/loaders/from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
 
+function G_leave_snippet()
+	if
+		((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+		and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+		and not require("luasnip").session.jump_active
+	then
+		require("luasnip").unlink_current()
+	end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command([[
+    autocmd ModeChanged * lua G_leave_snippet()
+]])
+
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━--
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
