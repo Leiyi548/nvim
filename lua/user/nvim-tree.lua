a8c7a8074fbd1d52e369ca3c75c986e70ee5cc2c
Leiyi548 <1424630446@qq.com>
2022-05-28 10:16:12 +0800

    Modify(nvim-tree): update configuration

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,58 +1,14 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
+-- custom icons
 local icons = require("user.icons")
+-- init.lua
 
-vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "S",
-		unmerged = "",
-		renamed = "➜",
-		deleted = "",
-		untracked = "U",
-		ignored = "◌",
-	},
-	--[[ git = {
-		unstaged = "!",
-		staged = "✓",
-		unmerged = "»",
-		renamed = "➜",
-		deleted = "✘",
-		untracked = "?",
-		ignored = "◌",
-	}, ]]
-	folder = {
-		arrow_open = "",
-		arrow_closed = "",
-		-- default = icons.documents.Folder,
-		-- open = icons.documents.OpenFolder,
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-		symlink_open = "",
-	},
-}
-
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-	vim.notify("nvim-tree not found!!!")
-	return
-end
-
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-	return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup({
+-- setup with all defaults
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+-- nested options are documented by accessing them with `.` (eg: `:help nvim-tree.view.mappings.list`).
+require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
 	auto_reload_on_write = true,
-	disable_netrw = true,
+	create_in_closed_folder = false,
+	disable_netrw = false,
 	hijack_cursor = false,
 	hijack_netrw = true,
 	hijack_unnamed_buffer_when_opening = false,
@@ -62,6 +18,9 @@ nvim_tree.setup({
 	open_on_tab = false,
 	sort_by = "name",
 	update_cwd = true,
+	reload_on_bufenter = false,
+	respect_buf_cwd = false,
+
 	view = {
 		width = 30,
 		height = 30,
@@ -82,6 +41,12 @@ nvim_tree.setup({
 		},
 	},
 	renderer = {
+
+		add_trailing = false,
+		group_empty = false,
+		highlight_git = true,
+		highlight_opened_files = "none",
+		root_folder_modifier = ":~",
 		indent_markers = {
 			enable = true,
 			icons = {
@@ -92,8 +57,42 @@ nvim_tree.setup({
 		},
 		icons = {
 			webdev_colors = true,
-			git_placement = "after", -- after | before
+			git_placement = "after", -- before | after
+			padding = " ",
+			symlink_arrow = " ➛ ",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+
+					arrow_closed = "",
+					arrow_open = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+			},
 		},
+		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
 	},
 	hijack_directories = {
 		enable = true,
@@ -111,8 +110,9 @@ nvim_tree.setup({
 	},
 	diagnostics = {
 		enable = true,
-		show_on_dirs = false,
+		show_on_dirs = true,
 		icons = {
+
 			hint = icons.diagnostics.Hint,
 			info = icons.diagnostics.Information,
 			warning = icons.diagnostics.Warning,
@@ -137,8 +137,10 @@ nvim_tree.setup({
 			restrict_above_cwd = false,
 		},
 		open_file = {
+
 			quit_on_open = false,
-			resize_window = false,
+
+			resize_window = true,
 			window_picker = {
 				enable = true,
 				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
@@ -153,6 +155,11 @@ nvim_tree.setup({
 		cmd = "trash",
 		require_confirm = true,
 	},
+	live_filter = {
+
+		prefix = "[FILTER]: ",
+		always_show_folders = true,
+	},
 	log = {
 		enable = false,
 		truncate = false,
@@ -160,14 +167,10 @@ nvim_tree.setup({
 			all = false,
 			config = false,
 			copy_paste = false,
+
 			diagnostics = false,
 			git = false,
 			profile = false,
 		},
 	},
-})
-
--- You can automatically close the tab/vim when nvim-tree is the last window in the tab.
--- vim.cmd([[
--- autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
--- ]])
+}) -- END_DEFAULT_OPTS
