723d31a4ff55cb785eb819e49cec29731aea4048
Leiyi548 <1424630446@qq.com>
2022-05-28 10:16:35 +0800

    Chore(theme): github-theme keyword_style NONE

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -152,7 +152,7 @@ require("github-theme").setup({
 	theme_style = "light_default",
 	comment_style = "italic",
 	function_style = "NONE",
-	keyword_style = "bold",
+	keyword_style = "NONE",
 	sidebars = { "qf", "vista_kind", "terminal", "packer" },
 
 	-- Change the "hint" color to the "orange" color, and make the "error" color bright red
