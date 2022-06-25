d71cd521ca7531b2e1221f71e91452538ec017b2
Leiyi548 <1424630446@qq.com>
2022-06-26 00:03:53 +0800

    发现neovim不适合自己回去用vscode和ide+vim了

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -114,31 +114,30 @@ if builtin.colorscheme.onedarkpro.active then
 		filetype_hlgroups = {}, -- Override default highlight groups for specific filetypes
 		plugins = { -- Override which plugins highlight groups are loaded
 			native_lsp = true,
-			polygot = true,
-			treesitter = true,
+			polygot = false,
 			-- NOTE: Other plugins have been omitted for brevity
 		},
 		styles = {
 			strings = "NONE", -- Style that is applied to strings
-			comments = "NONE", -- Style that is applied to comments
+			comments = "italic", -- Style that is applied to comments
 			keywords = "NONE", -- Style that is applied to keywords
 
 			functions = "NONE", -- Style that is applied to functions
 			variables = "NONE", -- Style that is applied to variables
-			virtual_text = "NONE", -- Style that is applied to virtual text
+			virtual_text = "italic", -- Style that is applied to virtual text
 		},
 		options = {
-			bold = false, -- Use the themes opinionated bold styles?
-			italic = false, -- Use the themes opinionated italic styles?
-			underline = false, -- Use the themes opinionated underline styles?
-			undercurl = false, -- Use the themes opinionated undercurl styles?
-			cursorline = false, -- Use cursorline highlighting?
-			transparency = false, -- Use a transparent background?
-			terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
-			window_unfocussed_color = false, -- When the window is out of focus, change the normal background?
+			bold = true, -- Use the themes opinionated bold styles?
+			italic = true, -- Use the themes opinionated italic styles?
+			underline = true, -- Use the themes opinionated underline styles?
+			undercurl = true, -- Use the themes opinionated undercurl styles?
+			cursorline = true, -- Use cursorline highlighting?
+			transparency = true, -- Use a transparent background?
+			terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+			window_unfocussed_color = true, -- When the window is out of focus, change the normal background?
 		},
 	})
-	-- onedarkpro.load()
+	onedarkpro.load()
 end
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --━━━━━━━━━━━━━━━━━━━━━❰ end onedarkpro configs ❱━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
