928029d89e9a0201ad0385539ccaa65beb9275c8
Leiyi548 <1424630446@qq.com>
2022-07-20 20:22:03 +0800

    feat: add vscode tab behaviours

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -8,8 +8,13 @@ local config = {}
 function config.nvim_lsp() end
 
 function config.nvim_cmp()
-  local ok, cmp = pcall(require, 'cmp')
-  if not ok then
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+  end
+
+  local cmp_ok, cmp = pcall(require, 'cmp')
+  if not cmp_ok then
     return
   end
   local lusnip_ok, luasnip = pcall(require, 'luasnip')
@@ -39,7 +44,7 @@ function config.nvim_cmp()
       ['<C-d>'] = cmp.mapping.scroll_docs(4),
       ['<PageUp>'] = cmp.mapping.scroll_docs(4),
       ['<PageDown>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-y>'] = cmp.mapping(cmp.mapping.complete(1), { 'i', 'c' }),
+      ['<C-y>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
       ['<C-]>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
       ['<C-e>'] = cmp.mapping({
         i = cmp.mapping.abort(),
@@ -51,6 +56,7 @@ function config.nvim_cmp()
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
       }),
+      -- like vscode tab behavior
       ['<Tab>'] = cmp.mapping(function(fallback)
         -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
         if cmp.visible() then
@@ -58,13 +64,40 @@ function config.nvim_cmp()
           if not entry then
             cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             cmp.confirm()
-          else
-            cmp.confirm()
           end
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
         else
           fallback()
         end
       end, { 'i', 's', 'c' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+        if cmp.visible() then
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.jump(-1)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { 'i', 's', 'c' }),
+      -- ['<Tab>'] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     local entry = cmp.get_selected_entry()
+      --     if not entry then
+      --       cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      --       cmp.confirm()
+      --   elseif luasnip.expand_or_jumpable() then
+      --     luasnip.expand_or_jump()
+      --   elseif has_words_before() then
+      --     cmp.complete()
+      --   else
+      --     fallback()
+      --   end
+      -- end, { 'i', 's' })
     }),
     formatting = {
       fields = { 'kind', 'abbr', 'menu' },
@@ -150,7 +183,7 @@ end
 function config.auto_pairs()
   -- Setup nvim-cmp.
   local auto_pairs_ok, npairs = pcall(require, 'nvim-autopairs')
-  if not  auto_pairs_ok then
+  if not auto_pairs_ok then
     return
   end
 
