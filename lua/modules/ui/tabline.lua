61d04086fd56ba78071aa339ecafdba3ac02e464
Leiyi548 <1424630446@qq.com>
2022-07-28 19:35:11 +0800

    bufferline show close icon

--- lua/modules/ui/tabline.lua
+++ lua/modules/ui/tabline.lua
@@ -5,7 +5,7 @@ end
 
 bufferline.setup({
   options = {
-    numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    numbers = 'ordinal', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
     close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
     right_mouse_command = 'veqrtical sbuffer %d', -- can be a string | function, see "Mouse actions"
     left_mouse_command = 'buffer %d', -- can be a string | function, see "Mouse actions"
@@ -16,6 +16,12 @@ bufferline.setup({
     close_icon = '',
     left_trunc_marker = '',
     right_trunc_marker = '',
+    name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+      -- remove extension from markdown files for example
+      if buf.name:match('%.md') then
+        return vim.fn.fnamemodify(buf.name, ':t:r')
+      end
+    end,
     max_name_length = 30,
     max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
     tab_size = 21,
@@ -29,11 +35,13 @@ bufferline.setup({
         highlight = 'BufferLineOffset',
       },
     },
+    ---@diagnostic disable-next-line: unused-local
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       return '(' .. count .. ')'
     end,
+    color_icons = true,
     show_buffer_icons = true,
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
