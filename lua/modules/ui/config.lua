04ee83e0d77e27e439f0fef7b7695864d9b7eb6f
Leiyi548 <1424630446@qq.com>
2022-09-10 10:23:50 +0800

    make tokyonight style =  night

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -31,7 +31,7 @@ function config.tokyonight()
   require('tokyonight').setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    style = 'storm', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+    style = 'night', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
     transparent = true, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
