f05c107fbe47365c9f4199cc232fb002c55b0cd4
Leiyi548 <1424630446@qq.com>
2022-10-29 07:41:58 +0800

    feat: change colorscheme according time

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 -- My favorite Colorscheme
 -- | 'github_light' | 'onedarkpro'  | 'tokyonight' | 'darkplus' | 'catppuccin' | 'zephyr' | 'rose-pine' | 'kanagawa' | 'gruvbox-baby' | 'gruvbox-material' | 'catppuccin' | 'gruvbox' | 'material' | 'juliana'
-_G_colorscheme = 'tokyonight'
+local _time = os.date('*t')
+if _time.hour > 18 or _time.hour < 6 then
+  _G_colorscheme = 'tokyonight-night'
+else
+  _G_colorscheme = 'tokyonight-day'
+end
 require('core')
 local ok, _ = pcall(vim.cmd, 'colorscheme ' .. _G_colorscheme)
 if not ok then
