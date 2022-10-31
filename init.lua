21e63dbf9856930dc06909da013e1c62b1a153af
Leiyi548 <1424630446@qq.com>
2022-10-31 21:45:29 +0800

    切换主题时间更早点

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
+-- justify system
 require('core')
 -- My favorite Colorscheme
 -- | 'github_light' | 'onedarkpro'  | 'tokyonight' | 'darkplus' | 'catppuccin' | 'zephyr' | 'rose-pine' | 'kanagawa' | 'gruvbox-baby' | 'gruvbox-material' | 'catppuccin' | 'gruvbox' | 'material' | 'juliana'
 local _time = os.date('*t')
-if _time.hour >= 17 or _time.hour < 6 then
+if _time.hour >= 16 or _time.hour < 6 then
   _G_colorscheme = 'tokyonight-night'
 else
   _G_colorscheme = 'tokyonight-day'
