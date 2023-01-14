89fdc010f2af375fc0669933fdc2b4eb3e9f49be
Leiyi548 <1424630446@qq.com>
2023-01-14 12:49:18 +0800

    feat(alpha): add startify bottom_buttons.val to open fugitive

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -1,1 +1,24 @@
-require 'alpha'.setup(require 'alpha.themes.startify'.config)
+local alpha = require 'alpha'
+local startify = require 'alpha.themes.startify'
+startify.section.header.val = {
+  [[                                   __                ]],
+  [[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
+  [[     / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+  [[    /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+  [[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+  [[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+}
+startify.section.top_buttons.val = {
+  startify.button("e", "  New file", ":ene <BAR> startinsert <cr>"),
+}
+
+startify.section.bottom_buttons.val = {
+  startify.button("g", "  Fugitive", ":tabnew | G<cr>"),
+  startify.button("q", "  Quit NVIM", ":qa<cr>"),
+}
+startify.section.footer = {
+  { type = "text", val = "footer" },
+}
+-- ignore filetypes in MRU
+
+alpha.setup(startify.config)
