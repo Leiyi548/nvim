a039492fc2fa1013a917ac236b973a18a3e99efe
Leiyi548 <1424630446@qq.com>
2023-01-14 14:56:32 +0800

    chore(keymap): use f open a new tab to run fugitive in alpha buffer
    * use f to instead g open new tab to run fugitive in alpha buffer

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -1,5 +1,6 @@
 local alpha = require 'alpha'
 local startify = require 'alpha.themes.startify'
+
 startify.section.header.val = {
   [[                                   __                ]],
   [[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -8,17 +9,18 @@ startify.section.header.val = {
   [[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
   [[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
+
 startify.section.top_buttons.val = {
   startify.button("e", "  New file", ":ene <BAR> startinsert <cr>"),
 }
 
 startify.section.bottom_buttons.val = {
-  startify.button("g", "  Fugitive", ":tabnew | G<cr>"),
+  startify.button("f", "  Fugitive", ":tabnew | G<cr>"),
   startify.button("q", "  Quit NVIM", ":qa<cr>"),
 }
+
 startify.section.footer = {
   { type = "text", val = "footer" },
 }
--- ignore filetypes in MRU
 
 alpha.setup(startify.config)
