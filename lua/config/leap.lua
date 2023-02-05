56fe03a9c9b7e4eb6fc19132815e66cc4339a09a
Leiyi548 <1424630446@qq.com>
2023-02-05 20:56:28 +0800

    chore(leap): make leap bidirectional search

--- lua/config/leap.lua
+++ lua/config/leap.lua
@@ -30,15 +30,17 @@ leap.setup({
   -- disable auto-jumping to the first match
   safe_labels = {},
 })
+-- leap default keymap
 -- require('leap').set_default_keymaps()
 
-vim.keymap.set('n', 's', '<Plug>(leap-forward)')
-vim.keymap.set('n', 'S', '<Plug>(leap-backward)')
-vim.keymap.set('x', 's', '<Plug>(leap-forward)')
-vim.keymap.set('o', 'z', '<Plug>(leap-forward)')
-vim.keymap.set('o', 'Z', '<Plug>(leap-backward)')
-vim.keymap.set('o', 'x', '<Plug>(leap-forward-x)')
-vim.keymap.set('o', 'X', '<Plug>(leap-backward-x)')
+vim.keymap.set('n', 's', '<cmd>lua require("leap").leap { target_windows = { vim.fn.win_getid() } }<cr>')
+vim.keymap.set('n', 'S', function()
+  require('leap').leap({
+    target_windows = vim.tbl_filter(function(win)
+      return vim.api.nvim_win_get_config(win).focusable
+    end, vim.api.nvim_tabpage_list_wins(0)),
+  })
+end)
+vim.keymap.set('o', 'z', '<cmd>lua require("leap").leap { target_windows = { vim.fn.win_getid() } }<cr>')
+vim.keymap.set('o', 'x', '<cmd>lua require("leap").leap { target_windows = { vim.fn.win_getid() } }<cr>')
 vim.keymap.set('n', 'gs', '<Plug>(leap-cross-window)')
-vim.keymap.set('x', 'gs', '<Plug>(leap-cross-window)')
-vim.keymap.set('o', 'gs', '<Plug>(leap-cross-window)')
