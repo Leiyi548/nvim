2d484f5ac5136f842b8e3cf54419a556d2065229
Leiyi548 <1424630446@qq.com>
2023-02-09 19:53:36 +0800

    feat(lsp): disable tsserver lsp fmt

--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -8,6 +8,9 @@ null_ls.setup({
   },
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client, bufnr)
+    if client.name == 'tsserver' then
+      client.server_capabilities.documentFormattingProvider = false
+    end
     if client.supports_method('textDocument/formatting') then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd('BufWritePre', {
