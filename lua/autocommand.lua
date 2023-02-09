4c611d87da0a37340a7b4560ab06bb07ce4f856f
Leiyi548 <1424630446@qq.com>
2023-02-09 20:00:54 +0800

    chore(fmt): stylua fmt

--- lua/autocommand.lua
+++ lua/autocommand.lua
@@ -84,9 +84,10 @@ vim.api.nvim_create_autocmd('ModeChanged', {
   pattern = '*',
   group = edit_group,
   callback = function()
-    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
-        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
-        and not require('luasnip').session.jump_active
+    if
+      ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+      and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require('luasnip').session.jump_active
     then
       require('luasnip').unlink_current()
     end
