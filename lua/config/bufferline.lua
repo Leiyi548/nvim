6c51a6717af7ceca9deb1369dbb7e577ec79409c
Leiyi548 <1424630446@qq.com>
2023-02-19 13:15:24 +0800

    fix(bufferline): make diagnostics = 'nvim_lsp'

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -33,7 +33,7 @@ bufferline.setup({
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     tab_size = 18,
-    diagnostics = 'coc', -- | "nvim_lsp" | "coc" | "none" ,
+    diagnostics = 'nvim_lsp', -- | "nvim_lsp" | "coc" | "none" ,
     diagnostics_update_in_insert = false,
     custom_filter = function(buf_number, buf_numbers)
       -- filter out filetypes you don't want to see
@@ -73,7 +73,7 @@ bufferline.setup({
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- more information please see bufferline-styling
-    separator_style = 'thin', -- slant | padded_slant | thick | thin (default)
+    separator_style = 'thick', -- slant | padded_slant | thick | thin (default)
     enforce_regular_tabs = false,
     always_show_bufferline = true,
     -- sort will change number,I don't need this.
