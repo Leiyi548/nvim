e8dddaee11647008d303ffc38e889311a28d7f22
Leiyi548 <1424630446@qq.com>
2023-03-10 13:11:07 +0800

    feat(nvim-hlsens): 使用 n 或 N 后居中

--- lua/config/nvim_hlsens.lua
+++ lua/config/nvim_hlsens.lua
@@ -3,13 +3,13 @@ local kopts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap(
   'n',
   'n',
-  [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR>zz<Cmd>lua require('hlslens').start()<CR>]],
   kopts
 )
 vim.api.nvim_set_keymap(
   'n',
   'N',
-  [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR>zz<Cmd>lua require('hlslens').start()<CR>]],
   kopts
 )
 vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
