d09e1dabff9f9a91be9e041c18de8a9ba789bd43
Leiyi548 <1424630446@qq.com>
2023-03-13 20:00:23 +0800

    fix(null_ls): 格式化只用 null_ls

--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -1,6 +1,16 @@
 local null_ls = require('null-ls')
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == 'null-ls'
+    end,
+    bufnr = bufnr,
+  })
+end
+
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
@@ -17,7 +27,7 @@ null_ls.setup({
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          lsp_formatting(bufnr)
         end,
       })
     end
